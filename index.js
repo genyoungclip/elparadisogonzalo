af6156f991c0d670b3ab4e2ee781ca567b74e353
genyoungclip <azehagowa@localhost>
2026-07-10 04:09:56 +0000

    Fix Node Docker configuration

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("."));
+
+app.listen(port, () => {
+  console.log(`Elparadisogonzalo running on port ${port}`);
+});
